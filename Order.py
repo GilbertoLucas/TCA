c267216642a6a6dce111dfcac5e05bf6b9599bf4
Gilberto Lucas <gilberto.santos@lccv.ufal.br>
2022-04-27 08:59:50 -0300

    Substituindo classe por conjunto de funções

--- Order.py
+++ Order.py
@@ -1,25 +1,29 @@
-
-
-
-class order(object):
-    def __init__(self) -> None:
-        pass
+def selection(lista):
+    x = lista
+    N = len(x)
+    for i in range(N-1):
+        id_menor = i
+        for j in range(i+1,N):
+            if(x[j] < x[id_menor]):
+                id_menor = j
+        x[i],x[id_menor] = x[id_menor],x[i]
     
-    def selection(self,lista):
-        x = lista
-
-        N = len(x)
-
-        for i in range(N-1):
-
-            id_menor = i
-
-            for j in range(i+1,N):
-                if(x[j] < x[id_menor]):
-                    id_menor = j
-
-
-            aux = x[i]
-            x[i] = x[id_menor]
-            x[id_menor] = aux
+def partition(lista,inicio,fim):
+    pivo = lista[fim]
+    i = inicio
+    for j in range(inicio,fim):
+        if lista[j] <= pivo:
+            lista[j],lista[i] = lista[i],lista[j]
+            i = i + 1
+    lista[i], lista[fim] = lista[fim], lista[i]
+    return i 
     
+    
+def quicksort(lista, inicio = 0,fim = None):
+
+    if fim is None:
+        fim = len(lista) - 1
+    if inicio < fim:
+        p = partition(lista,inicio,fim)
+        quicksort(lista,inicio,p-1)
+        quicksort(lista,p+1,fim)
