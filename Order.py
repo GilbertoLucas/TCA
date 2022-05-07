180a28dddab33d77fcfd5e3adec70c80c681bb87
Gilberto Lucas <31942165+GilbertoLucas@users.noreply.github.com>
2022-05-07 13:42:17 -0300

    Removendo partes antigas do código

--- Order.py
+++ Order.py
@@ -1,3 +1,8 @@
+# Algorimos classicos de ordenacao de lista
+# Autores: 
+# Gilberto Lucas 
+# Otavio Bruno
+
 def selection(lista, OrderedLists):
     if len(lista) == 0:
         return OrderedLists  # lista vazia
@@ -13,7 +18,6 @@ def selection(lista, OrderedLists):
 
     OrderedLists = x
     return OrderedLists
-
 
 def QuickSort(lists, OrderedLists, index=0):
     if len(lists) == 0:
@@ -39,7 +43,6 @@ def QuickSort(lists, OrderedLists, index=0):
     QuickSort(left_values, OrderedLists, index)  # recursao do lado esquerdo
 
     return OrderedLists
-
 
 def mergesort(x):
     n = len(x)
@@ -71,7 +74,6 @@ def mergesort(x):
 
     return x
 
-
 def insertion(x):
     n = len(x)
 
@@ -86,135 +88,3 @@ def insertion(x):
         x[j] = v
 
     return x
-
-
-# from wikipedia
-
-# def mergeSort(lista):
-#     if len(lista) > 1:
-#
-#         meio = len(lista) // 2
-#         # também é valido: meio = int(len(lista)/2)
-#
-#         listaDaEsquerda = lista[:meio]
-#         listaDaDireita = lista[meio:]
-#
-#         mergeSort(listaDaEsquerda)
-#         mergeSort(listaDaDireita)
-#
-#         i = 0
-#         j = 0
-#         k = 0
-#
-#         while i < len(listaDaEsquerda) and j < len(listaDaDireita):
-#
-#             if listaDaEsquerda[i] < listaDaDireita[j]:
-#                 lista[k] = listaDaEsquerda[i]
-#                 i += 1
-#             else:
-#                 lista[k] = listaDaDireita[j]
-#                 j += 1
-#             k += 1
-#
-#         while i < len(listaDaEsquerda):
-#             lista[k] = listaDaEsquerda[i]
-#             i += 1
-#             k += 1
-#
-#         while j < len(listaDaDireita):
-#             lista[k] = listaDaDireita[j]
-#             j += 1
-#             k += 1
-#     return lista
-
-# def partition(array, start, end):
-#     pivot = array[start]
-#     low = start + 1
-#     high = end
-#
-#     while True:
-#         # If the current value we're looking at is larger than the pivot
-#         # it's in the right place (right side of pivot) and we can move left,
-#         # to the next element.
-#         # We also need to make sure we haven't surpassed the low pointer, since that
-#         # indicates we have already moved all the elements to their correct side of the pivot
-#         while low <= high and array[high] >= pivot:
-#             high = high - 1
-#
-#         # Opposite process of the one above
-#         while low <= high and array[low] <= pivot:
-#             low = low + 1
-#
-#         # We either found a value for both high and low that is out of order
-#         # or low is higher than high, in which case we exit the loop
-#         if low <= high:
-#             array[low], array[high] = array[high], array[low]
-#             # The loop continues
-#         else:
-#             # We exit out of the loop
-#             break
-#
-#     array[start], array[high] = array[high], array[start]
-#
-#     return high
-#
-# def quick_sort(array, start, end):
-#     if start >= end:
-#         return
-#
-#     p = partition(array, start, end)
-#     quick_sort(array, start, p-1)
-#     quick_sort(array, p+1, end)
-
-# def QuickSort(arr):
-#     elements = len(arr)
-#
-#     # Base case
-#     if elements < 2:
-#         return arr
-#
-#     current_position = 0  # Position of the partitioning element
-#
-#     for i in range(1, elements):  # Partitioning loop
-#         if arr[i] <= arr[0]:
-#             current_position += 1
-#             temp = arr[i]
-#             arr[i] = arr[current_position]
-#             arr[current_position] = temp
-#
-#     temp = arr[0]
-#     arr[0] = arr[current_position]
-#     arr[current_position] = temp  # Brings pivot to it's appropriate position
-#
-#     left = QuickSort(arr[0:current_position])  # Sorts the elements to the left of pivot
-#     right = QuickSort(arr[current_position + 1:elements])  # sorts the elements to the right of pivot
-#
-#     arr = left + [arr[current_position]] + right  # Merging everything together
-#
-#     return arr
-
-
-# def quicksort(x, r=0, s=None):
-#     s = s if s is not None else len(x) - 1
-#     if r < s:
-#
-#         v = x[r]
-#         i = r
-#         j = s
-#
-#         while i <= j:
-#             while (x[i] < v):
-#                 i += 1
-#             while (x[j] > v):
-#                 j += -1
-#             if (i <= j):
-#                 aux = x[i]
-#                 x[i] = x[j]
-#                 x[j] = aux
-#                 i += 1
-#                 j += -1
-#
-#         quicksort(x, r, j)
-#         quicksort(x, i, s)
-#
-#     return x
